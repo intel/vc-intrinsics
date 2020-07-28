ef2f5a29901c3a21f917a2032dcf3d58944db373
Aleksander Us <aleksander.us@intel.com>
2020-07-28 17:34:40 +0300

    Unify usage of LLVM components in external and in-tree builds
    Use llvm components in both paths. Expand components to targets
    manually in external build. This allows to safely build plugins
    for that use intrinsics library statically in both build types.
    
    Change-Id: I388c9414e2001956a99a917e82ea15512ef39ecc
    CI-Depends: I388c9414e2001956a99a917e82ea15512ef39ecc

--- GenXIntrinsics/cmake/utils.cmake
+++ GenXIntrinsics/cmake/utils.cmake
@@ -3,16 +3,28 @@
 # included in dylib llvm targets should be replaced with LLVM
 # lib. Otherwise, just return passed libraries.
 # ret -- name of variable with returned targets list. All other
-# arguments are targets to process.
+# arguments are components to process.
 function(vc_get_llvm_targets RET)
   set(TARGETS ${ARGN})
+
   if (LLVM_LINK_LLVM_DYLIB)
+    # Drop all components, though it is probably not right
+    # and llvm_map_components_to_libnames should be used as filter.
+    # However, in external build it returns empty list for "all"
+    # so filtering is not really done.
     if ("${LLVM_DYLIB_COMPONENTS}" STREQUAL "all")
       set(TARGETS "")
     else()
       list(REMOVE_ITEM TARGETS ${LLVM_DYLIB_COMPONENTS})
     endif()
+  endif()
+
+  # Expand rest of components names to target names.
+  llvm_map_components_to_libnames(TARGETS ${TARGETS})
+
+  if (LLVM_LINK_LLVM_DYLIB)
     set(TARGETS ${TARGETS} LLVM)
   endif()
+
   set(${RET} ${TARGETS} PARENT_SCOPE)
 endfunction()
