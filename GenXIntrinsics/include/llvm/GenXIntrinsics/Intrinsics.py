cea24751619ae0dfe20316e9ce87d26844b90f85
lgotszal <lukasz.gotszald@intel.com>
2021-07-22 07:43:26 +0000

    update copyright headers update Python copyright to PEP8 style

--- GenXIntrinsics/include/llvm/GenXIntrinsics/Intrinsics.py
+++ GenXIntrinsics/include/llvm/GenXIntrinsics/Intrinsics.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-#=========================== begin_copyright_notice ============================
+# ========================== begin_copyright_notice ============================
 #
 # Copyright (C) 2019-2021 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 #
-#============================ end_copyright_notice =============================
+# =========================== end_copyright_notice =============================
 
 import os
 import sys
